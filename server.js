6b7d2a4008ebd83a4785f10e6021f7d703463ba7
tonymanpro <achavarria@impesa.net>
2018-11-09 16:11:31 -0600

    Version 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World versión 2!');
+  response.end('Hello World versión 2.1!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
